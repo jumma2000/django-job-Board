8d9554915371675139c35c64c8d13bde71b44a2a
jummaAli <jumma2000@hotmail.com.com>
2022-11-03 11:03:18 +0200

    change template

--- Job/urls.py
+++ Job/urls.py
@@ -3,6 +3,6 @@ from .import views
 
 urlpatterns = [
     path('',views.job_list,name='job_list'),
-    path('<int:id>',views.job_detail,name='job_detail'),
+    path('<int:id>',views.job_details,name='job_details'),
    
 ]
--- Job/views.py
+++ Job/views.py
@@ -1,10 +1,21 @@
+from contextvars import Context
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 
 
 def job_list(request):
-    return render(request,'job/index.html')
+    job_list = Job.objects.all()
+    
+    Context={'jobs':job_list}
+    
+    return render(request,'job/job_list.html',Context)
 
-def job_detail(requset,id):
-    pass
+
+
+def job_details(request , id):
+    job_details = Job.objects.get(id = id)  
+    Context={'job':job_details}
+    
+    return render(request,'job/job_details.html',Context)
